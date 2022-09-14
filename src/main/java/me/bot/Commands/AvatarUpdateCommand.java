bedc0fe5c386f6b3531d8cc30f3f6e02cda3eb0c
Igor Szafałowicz <igor.szafalowicz@o2.pl>
2022-09-14 11:03:09 +0200

    fixed avatar update

--- src/main/java/me/bot/Commands/AvatarUpdateCommand.java
+++ src/main/java/me/bot/Commands/AvatarUpdateCommand.java
@@ -7,6 +7,22 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 public class AvatarUpdateCommand extends ListenerAdapter {
+    
+    @Override
+    public void onGuildMemberUpdateAvatar(@NotNull GuildMemberUpdateAvatarEvent event) {
+        final String textChannelIdPlayground = "930860696332271690";
+        final String textChannelId = "667764000972472323"; //tlusty
+        final User user = event.getUser();
+
+        EmbedBuilder builder = new EmbedBuilder()
+                .setColor(0x78AAFF)
+                .setAuthor(user.getName(), event.getOldAvatarUrl(), event.getOldAvatarUrl())
+//                .setDescription("**Zmienił avatar** :open_mouth::point_right:")
+                .setDescription("**Zmienił avatar** :arrow_right: :arrow_right:")
+                .setThumbnail(event.getNewAvatarUrl());
+
+        event.getJDA().getGuildChannelById(textChannelId).getGuild().getDefaultChannel().asTextChannel().sendMessageEmbeds(builder.build()).queue();
+    }
 
     @Override
     public void onUserUpdateAvatar(@NotNull UserUpdateAvatarEvent event) {
